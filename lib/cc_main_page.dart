4a39eb5e541a2de2bf16cdca7268ce7fd7034bb7
Satyam Thakur <sat.thakur20@gmail.com>
2023-12-22 19:23:47 +0530

    Adding text field to take user input

--- lib/cc_main_page.dart
+++ lib/cc_main_page.dart
@@ -5,9 +5,44 @@ class AppStart extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    const borderStyle = OutlineInputBorder(
+      borderSide: BorderSide(width: 3, color: Colors.white), //<-- SEE HERE
+    );
+
+    return const Scaffold(
+      backgroundColor: Colors.blueGrey,
       body: Center(
-        child: Text("Hello, World"),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "0",
+              style: TextStyle(
+                  fontSize: 60,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white),
+            ),
+            Padding(
+              padding: EdgeInsets.all(16.0),
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: "Enter the amount in USD",
+                  hintStyle: TextStyle(color: Colors.white),
+                  prefixIcon: Icon(
+                    Icons.monetization_on_outlined,
+                    color: Colors.white,
+                  ),
+                  enabledBorder: borderStyle,
+                  focusedBorder: borderStyle,
+                ),
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+                keyboardType: TextInputType.numberWithOptions(decimal: true),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
